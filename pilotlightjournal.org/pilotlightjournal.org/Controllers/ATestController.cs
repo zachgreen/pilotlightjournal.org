5a58e2375be51b41b9c9a2705d2da158e793b222
unknown <Zach@.(none)>
2011-09-24 11:09:14 -0400

    More testing

---
+++ pilotlightjournal.org/pilotlightjournal.org/Controllers/ATestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pilotlightjournal.org.Controllers
+{
+    public class ATestController : Controller
+    {
+        //
+        // GET: /ATest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
