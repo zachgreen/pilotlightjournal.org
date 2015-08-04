495aa403ce3da2ec16fd0f4cbd3f66998d990084
Zach Green <zach.green@gmail.com>
2015-08-03 21:30:43 -0400

    Changes for september 2015 issue

--- pilotlightjournal.org/pilotlightjournal.org/Models/Partial.cs
+++ pilotlightjournal.org/pilotlightjournal.org/Models/Partial.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
-using System.Linq;
+﻿
 
-namespace pilotlightjournal.org.Models {
+using System.ComponentModel.DataAnnotations;
+
+namespace pilotlightjournal.org.Models
+{
     public class Partial {
         [Key]
         public int PartialId { get; set; }
 
-        [Required, MaxLength(50)]
+        [Required, System.ComponentModel.DataAnnotations.MaxLength(50)]
         public string Type { get; set; }
 
-        [Required, MaxLength(50)]
+        [Required, System.ComponentModel.DataAnnotations.MaxLength(50)]
         public string Name { get; set; }
 
         public virtual Work Work { get; set; }
