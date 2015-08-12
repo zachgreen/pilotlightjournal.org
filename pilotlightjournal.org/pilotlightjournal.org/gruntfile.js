0e15103a4554f78f98099d13d84458a039c87ecc
Zach Green <zach.green@gmail.com>
2015-08-11 20:12:53 -0400

    Adding more tracked files

---
+++ pilotlightjournal.org/pilotlightjournal.org/gruntfile.js
@@ -0,0 +1,33 @@
+﻿/// <binding AfterBuild='less' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        }, less: {
+            development: {
+                options: {
+                    paths: ["importfolder"]
+                },
+                files: {
+                    "Content/december_2013.css": "Content/december_2013.less",
+                    "Content/september_2015.css": "Content/september_2015.less"
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["bower:install"]);
+
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-less");
+};
